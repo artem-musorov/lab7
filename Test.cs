fc328ec411a6323d5b9db159a56e2e93f769f3d9
aksenia-studen <76278453+aksenia-studen@users.noreply.github.com>
2020-12-20 13:42:59 +0200

    Restored file Test.cs

---
+++ Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab10.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public string Email { get; set; }
+        public string Age { get; set; }
+        public string City { get; set; }
+    }
+}
