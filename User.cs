a101509b4871eeab7e7105eba402d89de7742f7a
aksenia-studen <76278453+aksenia-studen@users.noreply.github.com>
2020-12-20 12:02:38 +0200

    Create new branch

---
+++ User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab10.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public string Email { get; set; }
+        public string Age { get; set; }
+        public string City { get; set; }
+
+        public void SayHelloToUser(string textToAdd){
+            string str = "Hello, " + Name;
+            Console.WriteLine(str);
+        }
+    }
+}
